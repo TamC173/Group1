72f6c59242e03e9df4bc24c51b62392ffe4668fd
Daniel_Habetz <danielhabetz93@yahoo.com>
2015-06-08 15:22:59 -0500

    no real changes
    no real changes

--- root/Assets/NakedGame/NakedScripts/OnCollision.cs
+++ root/Assets/NakedGame/NakedScripts/OnCollision.cs
@@ -4,62 +4,56 @@ using System.Collections;
 
 public class OnCollision : MonoBehaviour {
 	
-	public int health = 4;
+	public int health = 4; //health of AI
 
 	public GameObject shirt;
 	public GameObject pants;
 	public GameObject shoes;
-	// Use this for initialization
+
+
 	void Start () 
 	{
-		shirt.SetActive (false);
+		/*shirt.SetActive (false); // sets shirt, pants, and shoes to be turned off
 		pants.SetActive (false);
-		shoes.SetActive (false);
+		shoes.SetActive (false); */
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 	}
-
-	/*void OnDamage() {
-		health --;
-		if (health <= 0) {
-			Destroy (gameObject);
-		}
-	}
-*/
+	
 	void OnCollisionEnter(Collision other)
 	{
 
-		print ("shot");
-		if (other.gameObject.tag != "bullet") return;
-
+		print ("shot"); //checks if AI was shot
+		if (other.gameObject.tag != "bullet")return;
+	
 			Destroy (other.gameObject);
 			//Destroy ();
 		print ("other tag is " + other.gameObject.tag);
 		health --;
 		if (health <= 0) {
 			Destroy (gameObject);
-			Application.LoadLevel("NakedVictory");
+			Application.LoadLevel("NakedVictory"); //when AI's health reaches 0, load "NakedVictory" scene.
 	}
 
-		if (health == 3)
+		/*if (health == 3)
 		{
-			shirt.SetActive(true);
+			shirt.SetActive(true); //AI's health decreases to 3, set shirt to active
 			//activate the shirt
 		}
 		if(health == 2)
 		{
-			pants.SetActive(true);
+			pants.SetActive(true);//AI's health decreases to 2, set pants to active
 			//activate the pants
 		}
 		if(health == 1)
 		{
-			shoes.SetActive(true);
+			shoes.SetActive(true);//AI's health decreases to 1, set shoes to active
 			//activate the shoes
 		}
-
+*/
 
   }
 
--- root/Assets/NakedGame/NakedScripts/ShootNew.cs
+++ root/Assets/NakedGame/NakedScripts/ShootNew.cs
@@ -10,20 +10,20 @@ public class ShootNew : MonoBehaviour
     public GUIText ammoText;
 	public Timer _timer;
 
+
  
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonUp("Fire1") && ammo > 0)
+        
+		if (Input.GetButtonUp("Fire1") && ammo > 0) // fires cannon, checks if cannon has fired
         {
-
-
 
           	GameObject clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
 			clone.transform.SetParent(gameObject.transform);
 
 
-			clone.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+			clone.GetComponent<Rigidbody>().AddForce(transform.forward * speed); //shoots bullets of gun's facing position
 			clone.transform.SetParent(null);
 
            //clone.transform = transform.TransformDirection(0, 0, speed);
@@ -35,13 +35,13 @@ public class ShootNew : MonoBehaviour
 			particle.Play();
             
             particle.enableEmission = true;
-			ammoText.text = "Ammo: " + ammo + "/" + maxAmmo;
+			ammoText.text = "Ammo: " + ammo + "/" + maxAmmo; // prints ammo counter to screen
 		
 		}
 
-		if (ammo == 0 || _timer.timerDone == true )
+		if (ammo == 0 || _timer.timerDone == true ) // if ammo is 0 or timer is done, load GameOver scene
 		{
-			Application.LoadLevel("NakedEnd");
+			Application.LoadLevel("NakedEnd"); // loads the GameOver Scene 
 		}
 		
 	}
